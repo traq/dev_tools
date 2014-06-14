9b293007584d1a2aec4ecfe2c70b39c7c9fc52ce
Jack Polgar <jack@polgar.id.au>
2014-06-14 19:06:44 +1000

    Setup compass for new theme.

---
+++ theme/config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir  = "sass"
+
+css_dir         = "../../vendor/Traq/Views/Default/CSS"
+images_dur      = "../../assets/images"
+javascripts_dir = "../../assets/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
